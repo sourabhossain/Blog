41cd64bc78445ad713e3cbc15e46cb40961fec94
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-10-21 12:47:17 +0600

    config

--- config/config.php
+++ config/config.php
@@ -1,6 +1,8 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "userdata");
 
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_NAME", "userdata");
+
+?>
