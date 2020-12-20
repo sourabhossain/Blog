2dff4ad77b8cfd5ba8859ce287e524e64cb02f4d
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-12-20 13:20:01 +0600

    one problem solve

--- inc/header.php
+++ inc/header.php
@@ -6,7 +6,7 @@
 	<meta name="description" content="It is a website about education">
 	<meta name="keywords" content="blog,cms blog">
 	<meta name="author" content="Delowar">
-	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">
+	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
 	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="style.css">
 	<script src="js/jquery.js" type="text/javascript"></script>
@@ -63,7 +63,7 @@ $(window).load(function() {
 <div class="navsection templete">
 	<ul>
 		<li><a id="active" href="index.html">Home</a></li>
-		<li><a href="about.html">About</a></li>
+		<li><a href="about.html">About</a></li>	
 		<li><a href="contact.html">Contact</a></li>
 	</ul>
 </div>
--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php 
 
 include("inc/header.php");
-include("inc/sidebar.php");
 
 ?>
+	
+<div class="slidersection templete clear">
+        <div id="slider">
+            <a href="#"><img src="images/slideshow/01.jpg" alt="nature 1" title="This is slider one Title or Description" /></a>
+            <a href="#"><img src="images/slideshow/02.jpg" alt="nature 2" title="This is slider Two Title or Description" /></a>
+            <a href="#"><img src="images/slideshow/03.jpg" alt="nature 3" title="This is slider three Title or Description" /></a>
+            <a href="#"><img src="images/slideshow/04.jpg" alt="nature 4" title="This is slider four Title or Description" /></a>
+        </div>
 
+</div>
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
 			<div class="samepost clear">
@@ -42,8 +50,66 @@ include("inc/sidebar.php");
 			</div>
 
 		</div>
+		<div class="sidebar clear">
+			<div class="samesidebar clear">
+				<h2>Categories</h2>
+					<ul>
+						<li><a href="#">Category One</a></li>
+						<li><a href="#">Category Two</a></li>
+						<li><a href="#">Category Three</a></li>
+						<li><a href="#">Category Four</a></li>
+						<li><a href="#">Category Five</a></li>						
+					</ul>
+			</div>
+			
+			<div class="samesidebar clear">
+				<h2>Latest articles</h2>
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+	
+			</div>
+			
+		</div>
+	</div>
 
-<?php 
-	include("inc/sidebar.php");
-	include("inc/footer.php");
-?>
+	<div class="footersection templete clear">
+	  <div class="footermenu clear">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Privacy</a></li>
+		</ul>
+	  </div>
+	  <p>&copy; Copyright Training with live project.</p>
+	</div>
+	<div class="fixedicon clear">
+		<a href="http://www.facebook.com"><img src="images/fb.png" alt="Facebook"/></a>
+		<a href="http://www.twitter.com"><img src="images/tw.png" alt="Twitter"/></a>
+		<a href="http://www.linkedin.com"><img src="images/in.png" alt="LinkedIn"/></a>
+		<a href="http://www.google.com"><img src="images/gl.png" alt="GooglePlus"/></a>
+	</div>
+<script type="text/javascript" src="js/scrolltop.js"></script>
+</body>
+</html>
