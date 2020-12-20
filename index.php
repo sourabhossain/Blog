ef43e24a3b9a48f30b13f047e5378488363d11a1
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-12-20 13:02:33 +0600

    sidebar

--- index.php
+++ index.php
@@ -42,47 +42,11 @@ include("inc/sidebar.php");
 			</div>
 
 		</div>
-		<div class="sidebar clear">
-			<div class="samesidebar clear">
-				<h2>Categories</h2>
-					<ul>
-						<li><a href="#">Category One</a></li>
-						<li><a href="#">Category Two</a></li>
-						<li><a href="#">Category Three</a></li>
-						<li><a href="#">Category Four</a></li>
-						<li><a href="#">Category Five</a></li>
-					</ul>
-			</div>
 
-			<div class="samesidebar clear">
-				<h2>Latest articles</h2>
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>
-					</div>
+		<?php 
+			include("inc/sidebar.php");
+		?>
 
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>
-					</div>
-
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>
-					</div>
-
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>
-					</div>
-
-			</div>
-
-		</div>
 	</div>
 
 	<div class="footersection templete clear">
