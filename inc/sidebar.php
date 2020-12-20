34ffc4948207785d13e5fafd7e62e0f41b123a79
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-12-20 13:24:05 +0600

    problme three solve

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -6,36 +6,36 @@
 						<li><a href="#">Category Two</a></li>
 						<li><a href="#">Category Three</a></li>
 						<li><a href="#">Category Four</a></li>
-						<li><a href="#">Category Five</a></li>
+						<li><a href="#">Category Five</a></li>						
 					</ul>
 			</div>
-
+			
 			<div class="samesidebar clear">
 				<h2>Latest articles</h2>
 					<div class="popular clear">
 						<h3><a href="#">Post title will be go here..</a></h3>
 						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
 					</div>
-
+					
 					<div class="popular clear">
 						<h3><a href="#">Post title will be go here..</a></h3>
 						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
 					</div>
-
+					
 					<div class="popular clear">
 						<h3><a href="#">Post title will be go here..</a></h3>
 						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
 					</div>
-
+					
 					<div class="popular clear">
 						<h3><a href="#">Post title will be go here..</a></h3>
 						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
 					</div>
-
+	
 			</div>
-
+			
 		</div>
