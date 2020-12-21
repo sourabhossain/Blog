e3de8ed9103554fd70165cae18013dc0ac5320bb
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-12-21 10:59:17 +0600

    post image show

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $fm = new Format();
 			<div class="samepost clear">
 				<h2><a href="post.php?id=<?php echo $result['id']; ?>"><?php echo $result['title']; ?></a></h2>
 				<h4>=<?php echo $fm->formatDate($result['date']); ?>, By <a href="#">=<?php echo $result['author']; ?></a></h4>
-				<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+				<a href="post.php?id=<?php echo $result['id']; ?>"><img src="admin/upload/<?php echo $result['image']; ?>" alt="post image"/></a>
 				=<?php echo $fm->textShorten($result['body']); ?>
 
 				<div class="readmore clear">
