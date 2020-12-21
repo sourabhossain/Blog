2cef297acad8d1876e1cd860d8007549d1939702
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-12-21 10:44:18 +0600

    textShorten

--- helpers/format.php
+++ helpers/format.php
@@ -4,6 +4,10 @@ class Format {
     public function formatDate($date) {
         return date('F j, Y, g:i a', strtotime($date));
     }
+
+    public function textShorten($body) {
+        
+    }
 }
 
 ?>
